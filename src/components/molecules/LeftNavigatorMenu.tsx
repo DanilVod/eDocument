471e2534e1df434a9a70d3f6639f990864bc5d68
DanilVod <danvodopyanov@yandex.ru>
2022-08-27 11:11:13 +0300

    test: ci3

---
+++ src/components/molecules/LeftNavigatorMenu.tsx
@@ -0,0 +1,13 @@
+import { Navigator } from '@/components/atoms'
+
+import { ROUTES } from '@/routes'
+
+export function LeftNavigatorMenu() {
+	return (
+		<>
+			<div className="w-1/6">
+				<Navigator routes={ROUTES}></Navigator>
+			</div>
+		</>
+	)
+}
